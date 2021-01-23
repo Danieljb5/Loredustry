c7b9272a89d931673d8f051f490233b982903b7e
Danieljb5 <39220451+Danieljb5@users.noreply.github.com>
2021-01-23 18:03:51 +0000

    Update krillan.js

--- scripts/krillan.js
+++ scripts/krillan.js
@@ -1,1 +1,15 @@
 
+const krillan = extend(Planet, "krillan", Planets.sun, 1, 0.5, {
+  generator: new SerpuloPlanetGenerator(),
+  bloom: true,
+  radius: 1,
+  accesible: false,
+  hasAtmosphere: true,
+  atmosphereColor: Color.valueOf("#ab7444"),
+  atmosphereRadIn: 0.02,
+  atmosphereRadOut: 0.03,
+  localizedName: "Krillan"
+});
+krillan.meshLoader = () => extend(HexMesh, krillan,2, {});
+
+const forgottenForest = extend(SectorPreset, "forgotten-forest"
