822f595f3b920e33644211441c92b7811dd3979c
Danieljb5 <39220451+Danieljb5@users.noreply.github.com>
2021-01-23 17:20:53 +0000

    Create examplePlanet.js

---
+++ scripts/examplePlanet.js
@@ -0,0 +1,21 @@
+// From the piexlcraft mod with excess core cut out
+
+const templura = extend(Planet, "templura", Planets.sun, 1, 0.5, {
+    generator: new SerpuloPlanetGenerator(),
+    bloom: true,
+    radius: 1,
+    accessible: true,
+    hasAtmosphere: true,
+    atmosphereColor: Color.valueOf("80ff00"),
+    atmosphereRadIn: 0.02,
+    atmosphereRadOut: 0.3,
+    localizedName: "Templura"
+});
+templura.meshLoader = () => extend(HexMesh, templura, 2, {});
+
+const rustedValley = extend(SectorPreset, "rusted-valley", templura, 1, {
+    captureWave: 50,
+    localizedName: "Rusted Valley",
+    difficulty: 3,
+    alwaysUnlocked: true
+});
