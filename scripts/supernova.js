274c7ae7d7f86afb7b0f1ef5a83ce18930120d63
Danieljb5 <danieljburfoot@outlook.com>
2021-06-09 17:23:15 +0100

    Added Star Forge

--- scripts/supernova.js
+++
@@ -1,8 +0,0 @@
-const supernova = extendContent(Block, "supernova", {
-    buildConfiguration(tile, table) {
-        table.addImageButton(Icon.arrowUpSmall, Styles.clearTransi, run(() => tile.configure(0))).size(50);
-    },
-    configured(tile, value) {
-        VREyeParameters.ui.hudfrag.showToast("Test.");
-    }
-})
